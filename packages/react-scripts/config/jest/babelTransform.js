527f4689e28456014fbe1a0e0c6f84d27e9d55c5
Christian Ost <c.ost@artirigo.de>
2017-06-27 13:29:20 +0200

    updates babel transform, updates jest config, bumps package version

--- packages/react-scripts/config/jest/babelTransform.js
+++ packages/react-scripts/config/jest/babelTransform.js
@@ -11,7 +11,21 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app'), 'stage-0'],
-  plugins: ['transform-decorators-legacy'],
+  presets: [
+    require.resolve('babel-preset-react-app'),
+    // added additional preset
+    require.resolve('babel-preset-stage-0'),
+  ],
+  // added all plugins
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+      },
+    ],
+    require.resolve('babel-plugin-transform-decorators-legacy'),
+    require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+  ],
   babelrc: false,
 });
